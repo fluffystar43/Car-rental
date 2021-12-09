ad5e2fd808e0f85202f1c22b2bf50bf286d9ddad
Artyom Shikhaleev <77292319+fluffystar43@users.noreply.github.com>
2021-12-09 21:52:14 +0300

    Обновлено приложение

--- desktop-app-csharp/client-application/Program.cs
+++ desktop-app-csharp/client-application/Program.cs
@@ -16,7 +16,7 @@ namespace client_application
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new formManager());
         }
     }
 }
