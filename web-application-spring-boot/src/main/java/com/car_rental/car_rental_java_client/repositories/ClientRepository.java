8c3c54cfe7a4cbe59d60896a84d896949c8f0311
Artyom Shikhaleev <77292319+fluffystar43@users.noreply.github.com>
2021-12-09 21:30:42 +0300

    Тест

--- web-application-spring-boot/src/main/java/com/car_rental/car_rental_java_client/repositories/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.car_rental.car_rental_java_client.repositories;
-
-import com.car_rental.car_rental_java_client.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-    Client findByLogin(String login);
-}
